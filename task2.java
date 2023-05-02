9459196693323bd162006aa46c7ed57bbfddcf95
Vasilev Kirill <kirya-vasilev-1994@mail.ru>
2023-05-02 06:09:46 +0300

    Домашняя работа по четвертому семинару

---
+++ task2.java
@@ -0,0 +1,28 @@
+
+//Реализуйте очередь с помощью LinkedList со следующими методами: enqueue() - помещает элемент в конец
+// очереди, dequeue() - возвращает первый элемент из очереди и удаляет его, first() - возвращает первый
+// элемент из очереди, не удаляя.
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class task2 {
+
+    public static void main (String[] args){
+        Queue<Integer> queue  = new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+        for (Integer integer: queue){
+            System.out.println(integer);
+    }
+
+}
+
+    private static void While(boolean b) {
+    }
+
+
+}
